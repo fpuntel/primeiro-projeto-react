92cff6a295bf070b76d980d48c00e898bdcc3c9c
Fernando <fernandopuntel@gmail.com>
2020-06-29 22:06:20 -0300

    first

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// Igual uma função
+// FC = function component
+const Dashboard: React.FC = () => {
+  return <h1>Dashboard</h1>;
+};
+
+export default Dashboard;
---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboard';
+import Repository from '../pages/Repository';
+
+const Routes: React.FC = () => (
+  // Utiliza o exact para testar exatamente a /
+  // caso contrário todas as outras rotas entrariam no
+  // dashbord
+  // Switch garante que apenas uma rota seja exibida
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repository" component={Repository} />
+  </Switch>
+);
+
+export default Routes;
